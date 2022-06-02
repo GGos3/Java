2a15ee6b015be431d09b02ce0534c283f6bfb0d6
GGos3 <janpll0421@gmail.com>
2022-06-02 10:01:12 +0900

    학교 자바 수업

--- StudentExam.java
+++ StudentExam.java
@@ -1,8 +1,13 @@
 public class StudentExam {
     public static void main(String[] args){
-        St kim = new St("김현우"); kim.grade = 2;
+        //St kim = new St("김현우"); kim.grade = 2;
+        //St jang = new St("장민재", 3);
+        //kim.print(); jang.print();
+        St kim = new St("김현우");
+        kim.setGrade(2);
         St jang = new St("장민재", 3);
-        kim.print(); jang.print();
+        System.out.println("학생의 이름은 " + kim.getName() + "이고, " + kim.setGrade() + "학년 입니다.");
+        System.out.println("학생의 이름은 " + jang.getName() + "이고, " + jang.setGrade() + "학년 입니다.");
     }
 }
 class St{
@@ -14,7 +19,19 @@ class St{
         this(name);
         this.grade = grade;
     }
-    public void print(){
+    public void print() {
         System.out.println("학생의 이름은 " + this.name + "이고 학년은 " + this.grade + "입니다.");
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public void setGrade (int grade){
+        this.grade = grade;
+    }
+    public int setGrade() {
+        return this.grade;
     }
 }
